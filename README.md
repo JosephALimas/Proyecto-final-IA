c1386e6f8945d9e7311a6906722162752efd45db
JosephALimas <0227551@up.edu.mx>
2024-05-15 09:22:31 -0600

    se agrega la funcion principal a la ventana principal, commit de prueba

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Proyecto-final-IA" 
+Instrucciones:
--- main_code.py
+++ main_code.py
@@ -1,3 +1,24 @@
-print("hello world")
-for i in range (0, 1, 5):
-    print(i)
+import pandas as pd
+import csv
+import sys
+import os
+from datetime import datetime
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from PyQt6.QtWidgets import QApplication, QCheckBox, QFrame,QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QLabel, QHBoxLayout, QComboBox, QTableWidget , QTableWidgetItem
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+################ CLASES ##################
+class MainMenuWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Finance UP")
+        self.setMinimumSize(500,300)
+################ MAIN ####################
+
+main_menu_window = MainMenuWindow()
+main_menu_window.show()
+deskTopApp.exec()
