0dbdf52e8a2a83b6f247932ef0b9cb976d214a61
Natalia Casarin <natalia.casarin02@gmail.com>
2024-05-26 13:45:08 -0600

    first commit

--- main_code.py
+++ main_code.py
@@ -3,11 +3,11 @@ import csv
 import sys
 import os
 from datetime import datetime
-import requests
+# import requests
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-from PyQt6.QtWidgets import QApplication, QCheckBox, QFrame,QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QLabel, QHBoxLayout, QComboBox, QTableWidget , QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QCheckBox, QFrame, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QLabel, QHBoxLayout, QComboBox, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
@@ -22,18 +22,20 @@ title_font.setBold(True)
 instr_font = QFont()
 instr_font.setFamily('Helvetica')
 instr_font.setPointSize(30)
-instr_font.setBold(True)    
+instr_font.setBold(True)
 
 instr2_font = QFont()
 instr2_font.setFamily('Helvetica')
 instr2_font.setPointSize(15)
-instr2_font.setBold(True)    
+instr2_font.setBold(True)
 ################ GUI CLASES ##################
+
+
 class MainMenuWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Finance UP")
-        self.setMinimumSize(920,1080)
+        self.setMinimumSize(920, 1080)
         self.main_menu_widget = QWidget(self)
         self.main_menu_widget.setStyleSheet("background-color: #F5F5F5;")
         self.main_menu_layout = QVBoxLayout(self.main_menu_widget)
@@ -65,13 +67,13 @@ class MainMenuWindow(QMainWindow):
         boton_opt_1.setStyleSheet('border: 2px solid #4B4B4B')
         boton_opt_1.clicked.connect(self.open_opt_1_window)
         boton_opt_1.setFixedHeight(250)
-        #boton de opcion dos
+        # boton de opcion dos
         boton_opt_2 = QPushButton("2. Ver análisis de gastos")
         boton_opt_2.setFont(instr_font)
         boton_opt_2.setStyleSheet('border: 2px solid #4B4B4B')
         boton_opt_2.setFixedHeight(250)
         boton_opt_2.clicked.connect(self.open_opt_2_window)
-        #boton de opcion tres
+        # boton de opcion tres
         boton_opt_3 = QPushButton("3. Predecir gastos")
         boton_opt_3.setFont(instr_font)
         boton_opt_3.setStyleSheet('border: 2px solid #4B4B4B')
@@ -100,16 +102,17 @@ class MainMenuWindow(QMainWindow):
     def open_opt_3_window(self):
         return None
 
+
 class Opt1Window(QWidget):
     def __init__(self, main_menu_window: MainMenuWindow):
         super().__init__()
         self.main_menu_window = main_menu_window
         self.setStyleSheet("background-color: #F5F5F5;")
         self.setWindowTitle("1. Ingresar gastos")
-        self.setMinimumSize(920,1080)
+        self.setMinimumSize(920, 1080)
         menu1_layout = QVBoxLayout(self)
 
-        #layout
+        # layout
         main_frame = QFrame()
         main_frame.setFrameShape(QFrame.Shape.Box)
         main_frame.setLineWidth(4)
@@ -117,7 +120,7 @@ class Opt1Window(QWidget):
         main_frame.setStyleSheet('color: #3f2b17;')
 
         title_frame = QFrame()
-        title_frame.setFrameShape(QFrame.Shape.Box) 
+        title_frame.setFrameShape(QFrame.Shape.Box)
         title_frame.setLineWidth(4)
         title_frame.setLayout(QVBoxLayout())
         title_frame.setStyleSheet('color: #2F2F2F;')
@@ -126,7 +129,6 @@ class Opt1Window(QWidget):
         welcome1_label.setStyleSheet('color: #191970;')
         welcome1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         welcome1_label.setFont(title_font)
-
 
         # regresar al menu principal
         return_button = QPushButton("Regresar al menú principal")
@@ -144,16 +146,17 @@ class Opt1Window(QWidget):
         self.main_menu_window.show()
         self.hide()
 
+
 class Opt2Window(QWidget):
-    def __init__(self, main_menu_window:MainMenuWindow):
+    def __init__(self, main_menu_window: MainMenuWindow):
         super().__init__()
         self.main_menu_window = main_menu_window
         self.setStyleSheet("background-color: #F5F5F5;")
         self.setWindowTitle("1. Ingresar gastos")
-        self.setMinimumSize(920,1080)
+        self.setMinimumSize(920, 1080)
         menu1_layout = QVBoxLayout(self)
 
-        #layout
+        # layout
         main_frame = QFrame()
         main_frame.setFrameShape(QFrame.Shape.Box)
         main_frame.setLineWidth(4)
@@ -161,7 +164,7 @@ class Opt2Window(QWidget):
         main_frame.setStyleSheet('color: #3f2b17;')
 
         title_frame = QFrame()
-        title_frame.setFrameShape(QFrame.Shape.Box) 
+        title_frame.setFrameShape(QFrame.Shape.Box)
         title_frame.setLineWidth(4)
         title_frame.setLayout(QVBoxLayout())
         title_frame.setStyleSheet('color: #2F2F2F;')
@@ -170,7 +173,6 @@ class Opt2Window(QWidget):
         welcome1_label.setStyleSheet('color: #191970;')
         welcome1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         welcome1_label.setFont(title_font)
-
 
         # regresar al menu principal
         return_button = QPushButton("Regresar al menú principal")
@@ -184,11 +186,10 @@ class Opt2Window(QWidget):
         title_frame.layout().addWidget(welcome1_label)
         main_frame.layout().addWidget(return_button)
 
-        
-
     def returnToMainMenu(self):
         self.main_menu_window.show()
         self.hide()
+
 
 ############## VARIABLES PRINCIPALES ##################
 deskTopApp = QApplication([])
