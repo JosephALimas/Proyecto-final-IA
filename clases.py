d2d336fb48d5b10a97824ba64b6163eeae84d1cc
Natalia Casarin <natalia.casarin02@gmail.com>
2024-05-28 00:52:11 -0600

    clases y menu test

--- clases.py
+++ clases.py
@@ -2,8 +2,9 @@ import datetime
 import pandas as pd
 import csv
 
+
 class Usuario:
-    def __init__(self,id = 0,nombre = "",contraseña = "",edad = 0,fecha_nacimiento = datetime, ingreso_mensual = ""):
+    def __init__(self, id=0, nombre="", contraseña="", edad=0, fecha_nacimiento=datetime, ingreso_mensual=""):
         self.id = id
         self.nombre = nombre
         self.contraseña = contraseña
@@ -11,8 +12,9 @@ class Usuario:
         self.fecha_nacimiento = fecha_nacimiento
         self.ingreso_mensual = ingreso_mensual
 
+
 class Gasto:
-    def __init__(self, id = 0,id_user = 0,articulo = "", precio = 0.0, importancia = 0, categoria = "" ,fecha = datetime):
+    def __init__(self, id=0, id_user=0, articulo="", precio=0.0, importancia=0, categoria="", fecha=datetime):
         self.id_gasto = id
         self.articulo = articulo
         self.id_user = id_user
@@ -20,3 +22,37 @@ class Gasto:
         self.precio = precio
         self.importancia = importancia
         self.fecha = fecha
+
+
+class FinancialBuddy:
+    def __init__(self):
+        self.gastos = []
+        self.ingresos = []
+
+    def menu(self):
+        while True:
+            print("\nMenu:")
+            print("1. Ingresar Gastos")
+            print("2. Ingresos")
+            print("3. Analizar Gastos")
+            print("4. Predecir Gastos")
+            print("5. Salir")
+            opcion = input("Seleccione una opción: ")
+
+            if opcion == '1':
+                self.ingresar_gastos()
+            elif opcion == '2':
+                self.ingresar_ingresos()
+            elif opcion == '3':
+                self.analizar_gastos()
+            elif opcion == '4':
+                self.predecir_gastos()
+            elif opcion == '5':
+                break
+            else:
+                print("Opción no válida. Por favor, intente de nuevo.")
+
+
+if __name__ == "__main__":
+    buddy = FinancialBuddy()
+    buddy.menu()
